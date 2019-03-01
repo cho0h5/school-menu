6b778d6f2c68ab4793fd4472a346503885255550
Youngho Cho <dominoyh5@gmail.com>
2019-03-01 16:07:18 +0900

    kakao API test passed

---
+++ crontab/README.md
@@ -0,0 +1,2 @@
+0 0 * * * node /yourpath/crontab/updateDayMenu.js
+0 0 * * 0 node /yourpath/crontab/updateWeekMenu.js
--- router/index.js
+++ router/index.js
@@ -1,6 +1,26 @@
+const keyboard = {
+    "type": "buttons",
+    "buttons": ["오늘의 메뉴", "내일의 메뉴"]
+}
+
+let menuRespond = {
+    "message": {
+        "text": "this is test"
+    },
+    "keyboard": keyboard
+}
+
 module.exports = (app) => {
 
     app.get('/', (req, res) => {
-        res.end("hello");
+
+    });
+
+    app.get('/keyboard', (req, res) => {
+        res.send(JSON.stringify(keyboard));
+    });
+
+    app.post('/message', (req, res) => {
+        res.send(JSON.stringify(menuRespond));
     });
 }
