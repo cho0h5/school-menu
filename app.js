c8a1f9de415b50b4cce389c3700e44835e9dd72a
Youngho Cho <dominoyh5@gmail.com>
2019-03-01 21:21:29 +0900

    Completed router/idnex.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 const port = 3000;
 
 app.use(express.json());
 
-var router = require('./router/index')(app);
+var router = require('./router/index')(app, fs);
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
--- router/index.js
+++ router/index.js
@@ -1,27 +1,51 @@
-const keyboard = {
-    "type": "buttons",
-    "buttons": ["오늘의 메뉴", "내일의 메뉴"]
-}
-
-let menuRespond = {
-    "message": {
-        "text": "this is test"
-    },
-    "keyboard": keyboard
-}
-
-module.exports = (app) => {
+module.exports = (app, fs) => {
 
     app.get('/', (req, res) => {
 
     });
 
+
     app.get('/keyboard', (req, res) => {
-        res.send(JSON.stringify(keyboard));
+        
+        const keyboardResponse = {
+            "type": "buttons",
+            "buttons": ["오늘의 메뉴", "내일의 메뉴"]
+        }
+        
+        res.send(JSON.stringify(keyboardResponse));
     });
 
+
     app.post('/message', (req, res) => {
-        console.log(req.body.content);
-        res.send(JSON.stringify(menuRespond));
+        
+        let menu = "";
+        if(req.body.content === "오늘의 메뉴")
+            menu = _getMenu(_getDay(), fs);
+        else
+            menu = _getMenu(_getDay()+1, fs);
+
+        let messageResponse = {
+            "message": {
+                "text": menu
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": ["오늘의 메뉴", "내일의 메뉴"]
+            }
+        }
+        
+        res.send(JSON.stringify(messageResponse));
     });
+}
+
+function _getDay(){
+    const date = new Date();
+    return date.getDay();
+}
+
+function _getMenu(day, fs){
+    let response = "";
+    let data = JSON.parse(fs.readFileSync('./data/menu.json', 'utf8'));
+    response = data["menu"][day];
+    return response;
 }
