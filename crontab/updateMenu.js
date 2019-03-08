059b77e34150c4863b4d064c53ef9fdfe195e60e
Youngho Cho <dominoyh5@gmail.com>
2019-03-09 00:38:05 +0900

    little succeed without .each() crontab/updateMenu.js

--- crontab/updateMenu.js
+++ crontab/updateMenu.js
@@ -1,9 +1,15 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-const myurl = 'https://stu.goe.go.kr/sts_sci_md01_001.do?schulCode=J100000749&schulCrseScCode=4&schulKndScCode=04';
+const myurl = 'https://stu.goe.go.kr/sts_sci_md00_001.do?schulCode=J100000749&schulCrseScCode=4&schulKndScCode=04';
 
 request.get(myurl, (error, response, body) => {
-    let $ = cheerio.load(body);
-    console.log($);
+    const $ = cheerio.load(body);
+
+    //$('#contents > div:nth-child(2) > table > tbody').each(() => {
+        
+    //    console.log($(this).children());
+    //});
+
+    console.log($('#contents > div:nth-child(2) > table > tbody').children().children().text());
 });
