c3edfe2958ce7c1421f7f34c09593cd50f46e9e7
Youngho Cho <dominoyh5@gmail.com>
2019-03-09 19:24:44 +0900

    succeed with .each()

--- crontab/updateMenu.js
+++ crontab/updateMenu.js
@@ -1,15 +1,17 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var regexr = /^\d*/;
 
 const myurl = 'https://stu.goe.go.kr/sts_sci_md00_001.do?schulCode=J100000749&schulCrseScCode=4&schulKndScCode=04';
 
 request.get(myurl, (error, response, body) => {
     const $ = cheerio.load(body);
 
-    //$('#contents > div:nth-child(2) > table > tbody').each(() => {
-        
-    //    console.log($(this).children());
-    //});
+    $('#contents > div:nth-child(2) > table > tbody > tr > td').each(function(){
+      
+        if(regexr.test($(this).text()))
+        console.log($(this).text());
+    });
 
-    console.log($('#contents > div:nth-child(2) > table > tbody').children().children().text());
+    //console.log($('#contents > div:nth-child(2) > table > tbody').children().children().text());
 });
