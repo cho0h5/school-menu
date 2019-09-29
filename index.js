20965b79042d6b85c6d96585e65dbc59490f4557
Youngho Cho <dominoyh5@gmail.com>
2019-09-29 22:21:39 +0900

    complete v3.0

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const moment = require('moment');
 
 const apiRouter = express.Router();
+
+const day = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일']
 
 app.use(logger('dev', {}));
 app.use(bodyParser.json());
@@ -21,7 +24,7 @@ apiRouter.post('/menu1', function(req, res){
             "outputs": [
                 {
                     "simpleText": {
-                        "text": "menu1 test"
+                        "text": _getXndMenu(1, fs)
                     }
                 }
             ]
@@ -38,7 +41,7 @@ apiRouter.post('/menu2', function(req, res){
             "outputs": [
                 {
                     "simpleText": {
-                        "text": "menu2 test"
+                        "text": _getXndMenu(2, fs)
                     }
                 }
             ]
@@ -47,6 +50,61 @@ apiRouter.post('/menu2', function(req, res){
 
     res.status(200).send(responseBody);
 });
+
+
+apiRouter.post('/menu3', function(req, res){
+    const responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": _getXndMenu(3, fs)
+                    }
+                }
+            ]
+        }
+    };
+
+    res.status(200).send(responseBody);
+});
+
+
+apiRouter.post('/menu4', function(req, res){
+    const responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": _getXndMenu(4, fs)
+                    }
+                }
+            ]
+        }
+    };
+
+    res.status(200).send(responseBody);
+});
+
+
+apiRouter.post('/menu5', function(req, res){
+    const responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": _getXndMenu(5, fs)
+                    }
+                }
+            ]
+        }
+    };
+
+    res.status(200).send(responseBody);
+});
+
 
 apiRouter.post('/today', function(req, res){
     const responseBody = {
@@ -86,7 +144,29 @@ app.listen(3000, function() {
     console.log('Server listening on port 3000!');
 });
 
-function _readMenu(month, day, fs){
+//test
+console.log(_getXndMenu(1, fs));
+console.log(_getXndMenu(2, fs));
+
+function _readMenu(date, fs){
     const data = JSON.parse(fs.readFileSync('menu.json', 'utf8'));
-    return data[month + '/' + day]
+    return data[date]
+}
+
+function _getXndMenu(Xnd, fs){
+    let addDate = 0;
+    let count = 0;
+    while(1){
+        date = moment().add(addDate, "days").format("MM/DD")
+        menu = _readMenu(date, fs);
+
+        if(menu == ""){
+            addDate++;
+            continue;
+        }
+        if(count == Xnd-1)
+            return date + "\n\n" + menu;
+        addDate++;
+        count++;
+    }
 }
--- loadMenu.py
+++ loadMenu.py
@@ -10,16 +10,16 @@ currentMonth = str(datetime.datetime.now().month)
 nextYear = str((datetime.datetime.now()+datetime.timedelta(days=30)).year)
 nextMonth = str((datetime.datetime.now()+datetime.timedelta(days=30)).month)
 
-if len(currentMonth) == 1:
+if len(currentMonth) == 1:      #month가 한자리면 앞에 0추가
     currentMonth = "0" + currentMonth
-if len(nextMonth) == 1:
+if len(nextMonth) == 1:     #month가 한자리면 앞에 0추가
     nextMonth = "0" + nextMonth
 
-URL = ["https://stu.goe.go.kr/sts_sci_md00_001.do?schulCode=J100000749&schulCrseScCode=4&schulKndScCode=04&ay="+currentYear+"&mm="+currentMonth, "https://stu.goe.go.kr/sts_sci_md00_001.do?schulCode=J100000749&schulCrseScCode=4&schulKndScCode=04&ay="+nextYear+"&mm="+nextMonth]
+URL = ["https://stu.goe.go.kr/sts_sci_md00_001.do?schulCode=J100000749&schulCrseScCode=4&schulKndScCode=04&ay="+currentYear+"&mm="+currentMonth, "https://stu.goe.go.kr/sts_sci_md00_001.do?schulCode=J100000749&schulCrseScCode=4&schulKndScCode=04&ay="+nextYear+"&mm="+nextMonth]        #각각 이번달, 다음달 급식 정보 링크
 
 finalMenu = {}
 
-def parseMenu(url, month):
+def parseMenu(url, month):      #크롤링해서 finalMenu dict에 추가
     response = requests.get(url)
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
@@ -28,14 +28,18 @@ def parseMenu(url, month):
     for i in soup:
         menu.append(str(i)[5:-6].split('<br/>'))
     for i in menu:
-        if len(i) >= 2:
-            finalMenu[month+'/'+i[0]] = '\n'.join(i[2:-1])
-        elif i[0].isdecimal():
-            finalMenu[month+'/'+i[0]] = ""
+        if i[0].isdecimal():        #날짜가 있는 테이블 칸인지
+            if len(i[0]) == 1:
+                i[0] = "0" + i[0]       #day가 한자리면 앞에 0추가
+
+            if len(i) >= 2:     #메뉴 있으면 메뉴넣고
+                finalMenu[month+'/'+i[0]] = '\n'.join(i[2:-1])
+            else:       #메뉴 없으면 ""넣고"
+                finalMenu[month+'/'+i[0]] = ""
 
 
-parseMenu(URL[0], currentMonth)
-parseMenu(URL[1], nextMonth)
+parseMenu(URL[0], currentMonth)     #이번달 메뉴 파싱
+parseMenu(URL[1], nextMonth)        #다음달 메뉴 파싱
 
-with open(file_path, 'w', encoding='utf-8') as file:
+with open(file_path, 'w', encoding='utf-8') as file:        #파일에 json으로 저장
     json_data = json.dump(finalMenu, file, indent='\t')
